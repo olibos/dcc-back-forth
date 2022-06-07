b9c86ca96b65042c143b710a71c6287e3ea4ab01
Olivier Bossaer <olivier.bossaer@gmail.com>
2022-06-07 13:37:10 +0200

    Setup base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/dcc-back-forth/',
   build:{
     outDir: 'docs',
     emptyOutDir: true,
